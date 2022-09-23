87006600fce47e647ef2d98d52f9f37a2100a96f
ramonyohannes <ramonebeyan@gmail.com>
2022-09-23 18:13:53 +0300

    feat: add increment and decrement counter functions

--- cubit/counter_state.dart
+++ cubit/counter_state.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'counter_cubit.dart';
 
-@immutable
-abstract class CounterState {}
-
-class CounterInitial extends CounterState {}
+class CounterState {
+  int counterValue;
+  CounterState({
+    required this.counterValue,
+  });
+}
